927b171b39c0f78c73c3edea053ad93657a272f8
Changguen Kim <rms1359@gmail.com>
2024-04-06 00:12:43 +0900

    seperate components and add card gradient

--- web/src/components/FullpageComponent.js
+++ web/src/components/FullpageComponent.js
@@ -2,6 +2,7 @@
 import ReactFullpage from "@fullpage/react-fullpage";
 import { useState } from 'react';
 
+import Coredata from './CoredataComponents.js'
 import Teampics from './TeampicComponents.js'
 
 import '../css/fullpage.css';
@@ -45,70 +46,7 @@ function Fullpagecomponents(){
           </div>
           <div className="section">
             <div className="slide" id="page2-1">
-              <p>Core Data</p>
-              <div className="grid-container">
-                <div className="grid-card card">
-                <p>RPM</p>
-                  <div class="card__content">
-                    <p class="card__title">RPM</p>
-                    <p class="card__description">바퀴의 분당 회전수를 나타내는 데이터</p>
-                    <button class="card__button">
-                      <a href="#">For More</a>
-                    </button>
-                    {/* <button class="card__button secondary">Source Code</button> */}
-                  </div>
-                </div>
-                <div className="grid-card card">
-                <p>Speed</p>
-                  <div class="card__content">
-                    <p class="card__title">Speed</p>
-                    <p class="card__description">속도 기록을 통해 차량이 얼마나 빠르게 가속했는지를 나타내는 데이터</p>
-                    <button class="card__button">
-                      <a href="#">For More</a>
-                    </button>
-                  </div>
-                </div>
-                <div className="grid-card card">
-                <p>Throttle Pos</p>
-                  <div class="card__content">
-                    <p class="card__title">Throttle Pos</p>
-                    <p class="card__description">엑셀 발판 위치를 나타내는 데이터</p>
-                    <button class="card__button">
-                      <a href="#">For More</a>
-                    </button>
-                  </div>
-                </div>
-                <div className="grid-card card">
-                <p>Timing Advance</p>
-                  <div class="card__content">
-                    <p class="card__title">Timing Advance</p>
-                    <p class="card__description">엔진 제어 시스템이 가속을 위해 연료의 점화 타이밍을 조정하고 있음을 나타내는 테이터</p>
-                    <button class="card__button">
-                      <a href="#">For More</a>
-                    </button>
-                  </div>
-                </div>
-                <div className="grid-card card">
-                <p>Engine Load</p>
-                  <div class="card__content">
-                    <p class="card__title">Engine Load</p>
-                    <p class="card__description">엔진이 많은 출력을 내고 있으며 차량이 빠르게 가속하고 있음을 나타내는 데이터</p>
-                    <button class="card__button">
-                      <a href="#">For More</a>
-                    </button>
-                  </div>
-                </div>
-                <div className="grid-card card">
-                  <p>Rapid Acceleration</p>
-                  <div class="card__content">
-                    <p class="card__title">Rapid Acceleration</p>
-                    <p class="card__description">운전자가 급가속을 했는지 여부를 직접적으로 나타내는 데이터</p>
-                    <button class="card__button">
-                      <a href="#">For More</a>
-                    </button>
-                  </div>
-                </div>
-              </div>
+              <Coredata></Coredata>
             </div>
             <div className="slide" id="page2-2">
               <p>page 2-2</p>
@@ -118,9 +56,7 @@ function Fullpagecomponents(){
             </div>
           </div>
           <div className="section" id="page3-1">
-
-<Teampics></Teampics>
-
+            <Teampics></Teampics>
           </div>
           <div className="section" id="page4-1">
             <p>4 page</p>
