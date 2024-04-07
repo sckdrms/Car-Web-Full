dd4c375d167bd6601691b8a9488f60d00232aa61
Changguen Kim <rms1359@gmail.com>
2024-04-07 20:33:18 +0900

    add footer

--- web/src/components/FullpageComponent.js
+++ web/src/components/FullpageComponent.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import ReactFullpage from "@fullpage/react-fullpage";
-import Coredata from './CoredataComponents'; // Coredata 컴포넌트 경로가 정확한지 확인하세요.
-import Teampics from './TeampicComponents'; // Teampics 컴포넌트 경로가 정확한지 확인하세요.
-import '../css/fullpage.css'; // CSS 파일 경로가 정확한지 확인하세요.
+import Coredata from './CoredataComponents'; 
+import Teampics from './TeampicComponents'; 
+import Footer from './FooterComponents'
+
+import '../css/fullpage.css';
 
 function FullpageComponents() {
   const [isSectionTwoVisible, setIsSectionTwoVisible] = useState(false);
@@ -23,7 +25,10 @@ function FullpageComponents() {
       render={({ state, fullpageApi }) => (
         <ReactFullpage.Wrapper>
           <div className="section">
-            <div className="slide" id="page1-1"><p className="page1-1-text">Team Alpha Circle</p></div>
+            <div className="slide" id="page1-1">
+              <p className="page1-1-text">Team
+              <p>Alpha Circle</p>
+            </p></div>
             <div className="slide" id="page1-2"><p>page 1-2</p></div>
           </div>
           <div className="section">
@@ -32,7 +37,7 @@ function FullpageComponents() {
             <div className="slide" id="page2-3"><p>page 2-3</p></div>
           </div>
           <div className="section" id="page3-1"><Teampics isVisible={isSectionThreeVisible} /></div>
-          <div className="section" id="page4-1"><p>4 page</p></div>
+          <div className="section" id="page4-1"><Footer style={{overflowY:'hidden'}}></Footer></div>
         </ReactFullpage.Wrapper>
       )}
     />
