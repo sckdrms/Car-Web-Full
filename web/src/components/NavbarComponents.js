1df97bc5f25c91b7b7622185a5e7a4c1f7076eb1
Changguen Kim <rms1359@gmail.com>
2024-04-08 00:23:36 +0900

    responsive nav design

--- web/src/components/NavbarComponents.js
+++ web/src/components/NavbarComponents.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import P404 from './404Components'
 import ReactFullpage from './FullpageComponent'
@@ -6,19 +6,37 @@ import '../css/navbar.css';
 import logo from '../img/identify/logo-1280x720.png';
 
 const Navbar = () => {
+
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  function toggleSidebar() {
+    setIsSidebarVisible(!isSidebarVisible);
+  }
+
   return (
     <BrowserRouter>
       <nav className='container'>
         <a href="/">        
           <img src={logo} className='logo' alt="logo" />
         </a>
+
+        <ul className={`sidebar ${isSidebarVisible ? '' : 'hidden'}`}>
+          <li onClick={toggleSidebar}><a href="/#"><svg style={{margin :'0px 49vw'}} xmlns="http://www.w3.org/2000/svg" fill='white' height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+          <li onClick={toggleSidebar}><a href="/#1">Home</a></li>
+          <li onClick={toggleSidebar}><a href="/#1/1">Product</a></li>
+          <li onClick={toggleSidebar}><a href="/#2">System</a></li>
+          <li onClick={toggleSidebar}><a href="/#3">Introduce</a></li>
+          <li onClick={toggleSidebar}><a href="/#4">Contact</a></li>
+        </ul>
+
         <ul>
-          <li><a href="/#1">Home</a></li>
-          <li><a href="/#1/1">Product</a></li>
-          <li><a href="/#2">System</a></li>
-          <li><a href="/#3">Introduce</a></li>
-          <li><a href="/#4">Contact</a></li>
-          <li><button className='loginbutton1'><a href="/#1">Log In</a></button></li>
+          <li className='hideOnMobile'><a href="/#1">Home</a></li>
+          <li className='hideOnMobile'><a href="/#1/1">Product</a></li>
+          <li className='hideOnMobile'><a href="/#2">System</a></li>
+          <li className='hideOnMobile'><a href="/#3">Introduce</a></li>
+          <li className='hideOnMobile'><a href="/#4">Contact</a></li>
+          <li className='hideOnMobile'><button className='loginbutton1'><a href="/#1">Log In</a></button></li>
+          <li className='hideOnPC' onClick={toggleSidebar}><a href="#"><svg style={{margin :'-10px 0px'}} xmlns="http://www.w3.org/2000/svg" fill='white' height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
         </ul>
       </nav>
       <Routes>
